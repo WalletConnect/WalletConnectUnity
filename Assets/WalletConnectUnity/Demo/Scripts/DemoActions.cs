e443cf83dd944f7e1fc00f5f91a4a2278e415e3a
Eddie <ecp4224@gmail.com>
2021-08-24 14:32:23 -0400

    Show Chain ID in demo scene

--- Assets/WalletConnectUnity/Demo/Scripts/DemoActions.cs
+++ Assets/WalletConnectUnity/Demo/Scripts/DemoActions.cs
@@ -30,7 +30,7 @@ public class DemoActions : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        accountText.text = "Connected:\n" + WalletConnect.ActiveSession.Accounts[0];
+        accountText.text = "\nConnected to Chain " + WalletConnect.ActiveSession.ChainId + ":\n" + WalletConnect.ActiveSession.Accounts[0];
     }
 
     public async void PersonalSign()
