f389e6deb3f58b620ca9f985c8fac345457a9f28
skibitsky <gleb@skibitsky.com>
2023-12-08 17:21:02 +0900

    Deep link loader animation

--- Packages/com.walletconnect.modal/Runtime/Views/ApprovalView/DeepLinkPage.cs
+++ Packages/com.walletconnect.modal/Runtime/Views/ApprovalView/DeepLinkPage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,6 +15,11 @@ namespace WalletConnectUnity.Modal.Views
         [SerializeField] private TMP_Text _titleText;
         [SerializeField] private string _titleTextFormat = "Continue in {0}";
 
+        [Header("Loader")]
+        [SerializeField] private float _radiansPerSecond = 1f;
+
+        [SerializeField] private RectTransform _loadingSector;
+
         public override Task InitializeAsync(
             Wallet wallet,
             WCModal modal,
@@ -29,13 +35,30 @@ namespace WalletConnectUnity.Modal.Views
             if (string.IsNullOrWhiteSpace(Uri)) return;
 
             StartCoroutine(OpenSessionProposalDeepLinkRoutine());
+            StartCoroutine(SectorRotationRoutine());
         }
-        
+
+        private void OnDisable()
+        {
+            StopAllCoroutines();
+            _loadingSector.gameObject.SetActive(false);
+        }
+
         private IEnumerator OpenSessionProposalDeepLinkRoutine()
         {
             // Skip one frame to not block the UI rendering
             yield return null;
             Linker.OpenSessionProposalDeepLink(Uri, Wallet);
+        }
+
+        private IEnumerator SectorRotationRoutine()
+        {
+            _loadingSector.gameObject.SetActive(true);
+            while (true)
+            {
+                _loadingSector.Rotate(0f, 0f, _radiansPerSecond * Time.deltaTime);
+                yield return null;
+            }
         }
     }
 }
