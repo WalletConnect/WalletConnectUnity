09dd996ab1f281ff3aab2ee2bac31939d63eaacb
skibitsky <gleb@skibitsky.com>
2023-12-25 13:39:34 +0900

    Corrected input field focus state

--- Packages/com.walletconnect.ui/Runtime/Components/WCInputField.cs
+++ Packages/com.walletconnect.ui/Runtime/Components/WCInputField.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace WalletConnectUnity.UI
@@ -14,6 +15,14 @@ namespace WalletConnectUnity.UI
         [SerializeField] private ComponentState _selectedState;
         [SerializeField] private ComponentState _highlightedState;
 
+        public override void OnSelect(BaseEventData eventData)
+        {
+            // Unity automatically 'selects' the input field after pressed which we don't want when using a cursor
+            var currentState = currentSelectionState;
+            if (currentState is SelectionState.Highlighted) return;
+
+            base.OnSelect(eventData);
+        }
 
         protected override void DoStateTransition(SelectionState state, bool instant)
         {
