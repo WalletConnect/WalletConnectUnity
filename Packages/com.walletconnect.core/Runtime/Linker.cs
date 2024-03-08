77013c99ada8c4feea6bce61773e2fd80da6b761
skibitsky <gleb@skibitsky.com>
2024-03-08 12:40:38 +0900

    Update Linker to be compatible with Jetifier and Firebase

--- Packages/com.walletconnect.core/Runtime/Linker.cs
+++ Packages/com.walletconnect.core/Runtime/Linker.cs
@@ -33,7 +33,7 @@ namespace WalletConnectUnity.Core
             if (string.IsNullOrWhiteSpace(uri))
                 throw new ArgumentException($"[Linker] Uri cannot be empty.");
 
-            uri = System.Uri.EscapeDataString(uri);
+            uri = Uri.EscapeDataString(uri);
 
 #if UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
             // In editor we cannot open _mobile_ deep links, so we just log the uri
@@ -63,14 +63,10 @@ namespace WalletConnectUnity.Core
                 throw new Exception("[Linker] No session topic found in provided session. Cannot open deep link.");
 
             if (session.Peer.Metadata is { Redirect: not null })
-            {
                 Application.OpenURL(session.Peer.Metadata.Redirect.Native);
-            }
             else
-            {
                 WCLogger.LogError(
                     $"[Linker] No redirect found for {session.Peer.Metadata.Name}. Cannot open deep link.");
-            }
         }
 
         public void OpenSessionRequestDeepLink(string sessionTopic)
@@ -104,17 +100,13 @@ namespace WalletConnectUnity.Core
             }, null);
         }
 
-        internal void OpenSessionRequestDeepLinkAfterMessageFromSession(string sessionTopic)
+        public void OpenSessionRequestDeepLinkAfterMessageFromSession(string sessionTopic)
         {
             WCLogger.Log($"[Linker] OpenSessionRequestDeepLinkAfterMessageFromSession. Topic: {sessionTopic}");
             if (_sessionMessagesCounter.TryGetValue(sessionTopic, out var messageCount))
-            {
                 _sessionMessagesCounter[sessionTopic] = messageCount + 1;
-            }
             else
-            {
                 _sessionMessagesCounter.Add(sessionTopic, 1);
-            }
         }
 
         public static bool CanOpenURL(string url)
@@ -158,10 +150,8 @@ namespace WalletConnectUnity.Core
         {
             if (disposed) return;
 
-            if (disposing)
-            {
-                _walletConnect.SignClient.Core.Relayer.Publisher.OnPublishedMessage -= OnPublisherPublishedMessage;
-            }
+            if (disposing) 
+_walletConnect.SignClient.Core.Relayer.Publisher.OnPublishedMessage -= OnPublisherPublishedMessage;
 
             disposed = true;
         }
