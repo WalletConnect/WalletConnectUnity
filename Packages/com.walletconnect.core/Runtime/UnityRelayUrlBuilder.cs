6b3d2de7e705367d34762b035e8b1a9448a01036
skibitsky <gleb@skibitsky.com>
2024-03-21 14:22:56 +0900

    Bump versions

--- Packages/com.walletconnect.core/Runtime/UnityRelayUrlBuilder.cs
+++ Packages/com.walletconnect.core/Runtime/UnityRelayUrlBuilder.cs
@@ -7,7 +7,7 @@ namespace WalletConnectUnity.Core
     public class UnityRelayUrlBuilder : RelayUrlBuilder
     {
         // TODO: Update this from CI
-        public const string PackageVersion = "3.0.6";
+        public const string PackageVersion = "3.1.0";
 
         public override (string name, string version) GetOsInfo()
         {
