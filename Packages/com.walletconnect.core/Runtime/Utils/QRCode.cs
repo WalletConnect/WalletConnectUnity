84b1ff315486916139f17efe3edddacf373e316a
skibitsky <gleb@skibitsky.com>
2024-06-06 15:43:57 +0300

    Compress QR code texture

--- Packages/com.walletconnect.core/Runtime/Utils/QRCode.cs
+++ Packages/com.walletconnect.core/Runtime/Utils/QRCode.cs
@@ -12,6 +12,8 @@ namespace WalletConnectUnity.Core.Utils
 
             var texture = new Texture2D(width, height);
             texture.SetPixels32(pixels);
+            texture.filterMode = FilterMode.Point;
+            texture.Compress(true);
             texture.Apply();
 
             return texture;
