8d96df21289a629af30087e1abb9e2a24446d4a3
skibitsky <gleb@skibitsky.com>
2024-06-19 11:45:57 +0300

    SdkMetadata static class

---
+++ Packages/com.walletconnect.core/Runtime/SdkMetadata.cs
@@ -0,0 +1,8 @@
+namespace WalletConnectUnity.Core
+{
+    public class SdkMetadata
+    {
+        public static string Type = "wcm";
+        public static string Version = "unity-sdk-v1.0.0";
+    }
+}
